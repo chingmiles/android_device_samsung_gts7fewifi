c87454302306796a80b395b0f87868a31c2f70f0
tungching0215 <62104163+tungching0215@users.noreply.github.com>
2024-04-12 22:57:34 +0800

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_gts7fewifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_gts7fewifi-eng
